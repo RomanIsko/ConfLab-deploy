56d13fa371f937c23ac57963a30a04d07ac05143
Roman Isko <iskoroman@gmail.com>
2016-01-29 11:46:17 +0200

    fix sh script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 cd app/
 adduser --create-home --system --shell /bin/bash jenkins
 sleep 3
-runuser -l jenkins java -jar "$(ls *.jar)" application.properties
+runuser -l jenkins -c 'java -jar "$(ls *.jar)" application.properties'
