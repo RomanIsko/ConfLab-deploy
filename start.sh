379aa54013143fb6ee23906270d502fd126e2687
Roman Isko <iskoroman@gmail.com>
2016-01-29 11:49:38 +0200

    revert to run as current user

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 cd app/
-adduser --create-home --system --shell /bin/bash jenkins
 sleep 3
-runuser -l jenkins -c 'java -jar "$(ls *.jar)" application.properties'
+java -jar "$(ls *.jar)" application.properties
